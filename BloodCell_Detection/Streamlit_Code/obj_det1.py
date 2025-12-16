0660688e3722ec2384dc2530d10770769926f07d
Lakshmi Mani Sankar Danda <171979917+DandaLakshmiManiSankar@users.noreply.github.com>
2025-12-16 17:47:59 +0530

    Update obj_det1.py

--- BloodCell_Detection/Streamlit_Code/obj_det1.py
+++ BloodCell_Detection/Streamlit_Code/obj_det1.py
@@ -4,6 +4,8 @@ from ultralytics import YOLO
 from PIL import Image, ImageDraw
 import numpy as np
 import pandas as pd
+import os
+os.environ["ULTRALYTICS_NO_CV2"] = "1"
 
 # Load trained YOLOv10 model
 MODEL_PATH = "/content/runs/detect/train/weights/best.pt"  # Update with actual path
@@ -44,22 +46,23 @@ if uploaded_file:
         st.dataframe(df)
     
     # Display precision and recall for each class and overall
-    st.write("### Precision and Recall")
-    metrics = model.val()  # Evaluate model performance
-    precision_list = metrics.box.p.tolist()
-    recall_list = metrics.box.r.tolist()
+    #st.write("### Precision and Recall")
+   # metrics = model.val()  # Evaluate model performance
+    #precision_list = metrics.box.p.tolist()
+    #ecall_list = metrics.box.r.tolist()
     
-    precision_recall_data = []
+    #precision_recall_data = []
     
-    for class_id, class_name in model.names.items():
-        precision = precision_list[class_id] if class_id < len(precision_list) else 0
-        recall = recall_list[class_id] if class_id < len(recall_list) else 0
-        precision_recall_data.append([class_name, precision, recall])
+    #for class_id, class_name in model.names.items():
+     #   precision = precision_list[class_id] if class_id < len(precision_list) else 0
+      #  recall = recall_list[class_id] if class_id < len(recall_list) else 0
+       # precision_recall_data.append([class_name, precision, recall])
     
     # Overall precision and recall
-    overall_precision = metrics.box.mp  # Mean precision
-    overall_recall = metrics.box.mr  # Mean recall
-    precision_recall_data.append(["Overall", overall_precision, overall_recall])
+   # overall_precision = metrics.box.mp  # Mean precision
+    #overall_recall = metrics.box.mr  # Mean recall
+    #precision_recall_data.append(["Overall", overall_precision, overall_recall])
     
-    df_metrics = pd.DataFrame(precision_recall_data, columns=["Class", "Precision", "Recall"])
-    st.dataframe(df_metrics)
+    #df_metrics = pd.DataFrame(precision_recall_data, columns=["Class", "Precision", "Recall"])
+    #st.dataframe(df_metrics)
+
